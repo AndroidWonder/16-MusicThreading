632ce184cdcfb4b4200bf6c86294debffb99e611
ClassroomAccount <jpepe44@gmail.com>
2020-02-28 14:43:42 -0500

    update rev

--- app/src/main/java/com/course/example/musicthread/MusicThreadActivity.java
+++ app/src/main/java/com/course/example/musicthread/MusicThreadActivity.java
@@ -15,6 +15,7 @@ import android.app.Activity;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.media.MediaPlayer;
+import android.util.Log;
 import android.widget.Toast;
 import android.widget.Button;
 import android.view.View;
@@ -53,19 +54,21 @@ public class  MusicThreadActivity extends Activity {
 
 		public void run(){
 			mp = new MediaPlayer();
+			mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
 				try {
 				//select Harvard stream
-				//mp.setDataSource("http://stream.whrb.org:8000/whrb-mp3");
+				mp.setDataSource("http://stream.whrb.org:8000/whrb-mp3");
 
 				//select BBC stream
-				mp.setDataSource("http://vprbbc.streamguys.net:80/vprbbc24.mp3");
+				//mp.setDataSource("http://vprbbc.streamguys.net:80/vprbbc24.mp3");
 
 				//select Russia
-                    // mp.setDataSource("http://radio-electron.ru:8000/96");
+				//	 mp.setDataSource("http://radio-electron.ru:8000/96");
 
 				mp.prepare();   // might take long! (for buffering, etc)
 				mp.start();
+
 				} catch (IOException e) {};
 
 		}
